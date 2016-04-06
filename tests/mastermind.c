f0483a7533f9f39dc66afceeb20c4f0bc0119428
Jérôme Pouiller <jezz@sysmic.org>
2016-04-06 10:49:35 +0200

    fixup computeSymetries()

--- tests/mastermind.c
+++ tests/mastermind.c
@@ -64,30 +64,35 @@ int getNumRealShots(shot_t shots[], int score) {
     return num;
 }
 
-int computeSymteries(shot_t shots[], colorlist_t *colors) {
+// Given number of unknown colors and last jocker used, compute symetries of a
+// shot.
+static int _computeSymetries(shot_t *shot, char lst_jocker, int nb_unknown) {
+    int i;
+    int cnt = 1;
+    for (i = 0; i < NB_PLACES; i++) {
+        if (shot->d[i] > lst_jocker && shot->d[i] < JOCKER_OFFSET + NB_COLORS) {
+            lst_jocker = shot->d[i];
+            cnt *= nb_unknown;
+            nb_unknown--;
+        }
+    }
+    assert(cnt > 0);
+    return cnt;
+}
+
+int computeSymetries(shot_t shots[], colorlist_t *colors) {
     int total = 0;
     int i, j;
-    int nb_unknown_orig;
-    char lst_jocker_orig = JOCKER_OFFSET - 1;
+    int nb_unknown;
+    char lst_jocker = JOCKER_OFFSET - 1;
 
     for (i = 0; colors->d[i]; i++)
-        if (colors->d[i] > lst_jocker_orig && colors->d[i] < JOCKER_OFFSET + NB_COLORS)
-            lst_jocker_orig = colors->d[i];
-    nb_unknown_orig = NB_COLORS - i;
+        if (colors->d[i] > lst_jocker && colors->d[i] < JOCKER_OFFSET + NB_COLORS)
+            lst_jocker = colors->d[i];
+    nb_unknown = NB_COLORS - i;
     for (i = 0; shots[i].d[0]; i++) {
-        char lst_jocker = lst_jocker_orig;
-        int nb_unknown = nb_unknown_orig;
-        int cnt = 1;
-        for (j = 0; j < NB_PLACES; j++) {
-            if (shots[i].d[j] > lst_jocker && shots[i].d[j] < JOCKER_OFFSET + NB_COLORS) {
-                lst_jocker = shots[i].d[j];
-                cnt *= nb_unknown;
-                nb_unknown--;
-            }
-        }
-        assert(cnt > 0);
-        shots[i].d[IDX_NUM_SYM] = cnt;
-        total += cnt;
+        shots[i].d[IDX_NUM_SYM] = _computeSymetries(shots + i, lst_jocker, nb_unknown);
+        total += shots[i].d[IDX_NUM_SYM];
     }
     return total;
 }
